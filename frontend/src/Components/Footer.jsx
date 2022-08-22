f55cfd1fc38da5f9f607d9adf9b35bd39c585c84
Elsa <100567053+elsa5152@users.noreply.github.com>
2022-08-22 13:35:02 +0930

    basic

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,49 @@
+const Footer = () => (
+    <footer className="text-center text-white" style={{backgroundColor: "#f1f1f1"}}>
+    <div className="container pt-6">
+        <section className="mb-6">
+        <a
+            target="_blank" rel="noopener noreferrer"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="https://www.facebook.com/"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i className="fab fa-facebook-f"></i>
+        </a>
+
+        <a
+            target="_blank" rel="noopener noreferrer"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="https://www.instagram.com/"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i className="fab fa-instagram"></i>
+        </a>
+
+        <a
+            target="_blank" rel="noopener noreferrer"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="https://www.linkedin.com/"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i className="fab fa-linkedin"></i>
+        </a>
+
+        <a
+            target="_blank" rel="noopener noreferrer"
+            className="btn btn-link btn-floating btn-lg text-dark m-1"
+            href="https://github.com/elsa5152/PROJECT3-lotteryapp.git"
+            role="button"
+            data-mdb-ripple-color="dark"
+            ><i className="fab fa-github"></i>
+        </a>
+        </section>
+    </div>
+
+    <div className="text-center text-pink p-2" style={{backgroundColor: "pink"}}>
+        <a className="text-pink"> Elsa</a>
+    </div>
+    </footer>
+);
+
+export default Footer;
